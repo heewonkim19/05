ba788fec7382d412c565de8725d5c44b1b4ba38c
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-10-08 15:30:02 +0900

    project05

--- main.c
+++ main.c
@@ -5,15 +5,10 @@
 
 int main(int argc, char *argv[]) 
 {
-    int i=0, meter;
+    int i;
+    for (i=0; i<10; i++)
+      printf("Hello world!\n"); 
     
-    while (i<3)
-    {
-    	meter=i*1609;
-    	printf ("%d mile is %d meter.\n",i,meter);
-    	i++;
-	}
-
 	return 0;
 }
 
